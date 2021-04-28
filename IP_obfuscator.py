158cf30f790a7567b8143f9aa69be8e89bb1457c
ak <ak>
2021-04-28 14:42:34 +0530

    Ip-obfuscation

---
+++ IP_obfuscator.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+import argparse
+import re
+import sys
+import ipaddress
+
+def obfuscate_ip(ip):
+    print('[~] Obfuscated IPs:\n')
+    for match in re.finditer(r'((?P<a>\d+)\.)((?P<b>\d+)\.)((?P<c>\d+)\.)'
+                             '(?P<d>\d+)', ip):
+        print('[+] http://'+str(int(match.group('a'))*256**3 +
+                                int(match.group('b'))*256**2 +
+                                int(match.group('c'))*256 +
+                                int(match.group('d'))))
+        print('[+] http://'+str(hex(int(match.group('a'))*256**3 +
+                                int(match.group('b'))*256**2 +
+                                int(match.group('c'))*256 +
+                                int(match.group('d')))))
+        print('[+] http://'+re.sub('o', '', str(oct(int(match.group('a')) *
+                                   256**3+int(match.group('b'))*256**2 +
+                                   int(match.group('c'))*256 +
+                                   int(match.group('d')))))+'\n')
+        print('[+] http://'+re.sub('o', '', str(oct(int(match.group('a')))) +
+                                   '.'+str(oct(int(match.group('b'))))+'.' +
+                                   str(oct(int(match.group('c'))))+'.' +
+                                   str(oct(int(match.group('d'))))))
+        print('[+] http://'+re.sub('o', '0000000',
+                                   str(oct(int(match.group('a'))))+'.' +
+                                   str(oct(int(match.group('b'))))+'.' +
+                                   str(oct(int(match.group('c'))))+'.' +
+                                   str(oct(int(match.group('d'))))))
+        print('[+] http://'+str(hex(int(match.group('a'))))+'.' +
+              str(hex(int(match.group('b'))))+'.' +
+              str(hex(int(match.group('c'))))+'.' +
+              str(hex(int(match.group('d')))))
+        print('[+] http://'+re.sub('x', 'x00000000',
+              str(hex(int(match.group('a'))))+'.' +
+              str(hex(int(match.group('b'))))+'.' +
+              str(hex(int(match.group('c'))))+'.' +
+              str(hex(int(match.group('d')))))+'\n')
+        print('[+] http://'+str(hex(int(match.group('a'))))+'.' +
+              str(hex(int(match.group('b'))))+'.' +
+              str(hex(int(match.group('c'))))+'.'+match.group('d'))
+        print('[+] http://'+str(hex(int(match.group('a'))))+'.' +
+              str(hex(int(match.group('b'))))+'.' +
+              match.group('c')+'.'+match.group('d'))
+        print('[+] http://'+str(hex(int(match.group('a'))))+'.' +
+              match.group('b')+'.'+match.group('c')+'.'+match.group('d'))
+        print('[+] http://'+match.group('a')+'.' +
+              str(hex(int(match.group('b'))))+'.' +
+              str(hex(int(match.group('c'))))+'.' +
+              str(hex(int(match.group('d')))))
+        print('[+] http://'+match.group('a')+'.'+match.group('b')+'.' +
+              str(hex(int(match.group('c'))))+'.' +
+              str(hex(int(match.group('d')))))
+        print('[+] http://'+match.group('a')+'.'+match.group('b')+'.' +
+              match.group('c')+'.'+str(hex(int(match.group('d'))))+'\n')
+        print('[+] http://'+re.sub('o', '', str(oct(int(match.group('a')))) +
+              '.'+str(oct(int(match.group('b'))))+'.' +
+              str(oct(int(match.group('c'))))+'.'+match.group('d')))
+        print('[+] http://'+re.sub('o', '', str(oct(int(match.group('a')))) +
+              '.'+str(oct(int(match.group('b'))))+'.' +
+              match.group('c')+'.'+match.group('d')))
+        print('[+] http://'+re.sub('o', '', str(oct(int(match.group('a')))) +
+              '.'+match.group('b')+'.'+match.group('c')+'.'+match.group('d')))
+        print('[+] http://'+re.sub('o', '', match.group('a')+'.' +
+              str(oct(int(match.group('b'))))+'.' +
+              str(oct(int(match.group('c'))))+'.' +
+              str(oct(int(match.group('d'))))))
+        print('[+] http://'+re.sub('o', '', match.group('a')+'.' +
+              match.group('b')+'.'+str(oct(int(match.group('c'))))+'.' +
+              str(oct(int(match.group('d'))))))
+        print('[+] http://'+re.sub('o', '', match.group('a')+'.' +
+              match.group('b')+'.'+match.group('c')+'.' +
+              str(oct(int(match.group('d')))))+'\n')
+        print('[+] http://'+str(hex(int(match.group('a'))))+'.' +
+              str(hex(int(match.group('b'))))+'.'+str(int(match.group('c')) *
+              256+int(match.group('d'))))
+        print('[+] http://'+str(hex(int(match.group('a'))))+'.' +
+              str(int(match.group('b'))*256**2+int(match.group('c'))*256 +
+              int(match.group('d'))))
+        print('[+] http://'+re.sub('o', '', str(oct(int(match.group('a')))) +
+              '.'+str(oct(int(match.group('b')))))+'.' +
+              str(int(match.group('c'))*256+int(match.group('d'))))
+        print('[+] http://'+re.sub('o', '', str(oct(int(match.group('a'))))) +
+              '.'+str(int(match.group('b'))*256**2+int(match.group('c'))*256 +
+                      int(match.group('d'))))
+        print('[+] http://'+str(hex(int(match.group('a'))))+'.' +
+              re.sub('o', '', str(oct(int(match.group('b')))))+'.' +
+              str(int(match.group('c'))*256+int(match.group('d'))))
+        print('[+] http://'+re.sub('o', '', str(oct(int(match.group('a'))))) +
+              '.'+str(hex(int(match.group('b'))))+'.' +
+              str(int(match.group('c'))*256+int(match.group('d')))+'\n')
+
+        print('IPv4 mapped IPv6 addresses: ')
+        print('[+] http://'+'::ffff:'+str(hex(int(match.group('a'))*256**3 +
+              int(match.group('b'))*256**2+int(match.group('c'))*256 +
+              int(match.group('d'))))[2:])
+        print('[+] http://'+'0:0:0:0:0:ffff:'+str(hex(int(match.group('a')) *
+              256**3+int(match.group('b'))*256**2+int(match.group('c'))*256 +
+              int(match.group('d'))))[2:])
+        print('[+] http://'+'0000:0000:0000:0000:0000:ffff:' +
+              str(hex(int(match.group('a'))*256**3+int(match.group('b'))*256 **
+                  2+int(match.group('c'))*256+int(match.group('d'))))[2:])
+        print('[+] http://'+'0000:0000:0000:0000:0000:ffff:'+ip+'\n')
+
+ip = sys.argv[1]
+try:
+      print("Entered IP: ",ip)
+      int(ipaddress.ip_address(ip))
+      obfuscate_ip(ip)
+except ValueError:
+      print('Error: wrong IP address format.')
+      print('Only in normal CIDR notation. eg:127.0.0.1')
+      sys.exit(2)
---
+++ README.md
@@ -0,0 +1,52 @@
+# IP Obfuscator
+Simple script you can use to convert and obfuscate any IP address of any host.
+
+## Usage
+```
+usage: IP_obfuscator.py <ip>
+
+```
+
+## Example
+```
+$ python3 IP_obfuscator.py 127.0.0.1
+Entered IP:  127.0.0.1
+[~] Obfuscated IPs:
+
+[+] http://2130706433
+[+] http://0x7f000001
+[+] http://017700000001
+
+[+] http://0177.00.00.01
+[+] http://00000000177.000000000.000000000.000000001
+[+] http://0x7f.0x0.0x0.0x1
+[+] http://0x000000007f.0x000000000.0x000000000.0x000000001
+
+[+] http://0x7f.0x0.0x0.1
+[+] http://0x7f.0x0.0.1
+[+] http://0x7f.0.0.1
+[+] http://127.0x0.0x0.0x1
+[+] http://127.0.0x0.0x1
+[+] http://127.0.0.0x1
+
+[+] http://0177.00.00.1
+[+] http://0177.00.0.1
+[+] http://0177.0.0.1
+[+] http://127.00.00.01
+[+] http://127.0.00.01
+[+] http://127.0.0.01
+
+[+] http://0x7f.0x0.1
+[+] http://0x7f.1
+[+] http://0177.00.1
+[+] http://0177.1
+[+] http://0x7f.00.1
+[+] http://0177.0x0.1
+
+IPv4 mapped IPv6 addresses:
+[+] http://::ffff:7f000001
+[+] http://0:0:0:0:0:ffff:7f000001
+[+] http://0000:0000:0000:0000:0000:ffff:7f000001
+[+] http://0000:0000:0000:0000:0000:ffff:127.0.0.1
+
+```
